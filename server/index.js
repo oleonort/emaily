c3aff0ecc6fd13a9eec0048fd6ad947e5a5ce04a
oleonort <oleonortt@gmail.com>
2017-10-12 00:05:00 +0200

    first commit

---
+++ server/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (request, response) => {
+    response.send({ hi: 'there' });
+});
+
+// deplouing with heroku.com
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
