ec814c8686bcdef1c32fe4e98342953cd2402ef4
oleonort <oleonortt@gmail.com>
2017-10-12 00:34:58 +0200

    test change

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (request, response) => {
-    response.send({ hi: 'there' });
+    response.send({ hi: 'there!!!' });
 });
 
 // deplouing with heroku.com
