e271a02bceb11054f533e4d2a0970124faec2a8c
oleonort <oleonortt@gmail.com>
2017-10-29 22:06:53 +0100

    google oauth

---
+++ server/routes/authRoutes.js
@@ -0,0 +1,19 @@
+const passport = require('passport');
+
+module.exports = app => {
+    app.get('/auth/google', passport.authenticate('google', {
+        scope: ['profile', 'email']
+    }));
+
+    app.get('/auth/google/callback', passport.authenticate('google'));
+
+    app.get('/api/logout', (requst, response) => {
+        // logout is attached automatically by passport
+        requst.logout();
+        response.send(requst.user);
+    });
+
+    app.get('/api/current-user', (requst, response) => {
+        response.send(requst.user);
+    });
+}
---
+++ server/services/passport.js
@@ -0,0 +1,34 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
+const keys = require('../config/keys');
+
+// model class -> represents collection in DB, but is a class in JS
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id).then(user => done(null, user));
+});
+
+passport.use(
+    new GoogleStrategy({
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        callbackURL: '/auth/google/callback'
+    }, (accessToken, refreshToken, profile, done) => {
+        // query returns a promise
+        User.findOne({ googleId: profile.id }).then(existingUser => {
+            if (existingUser) {
+                done(null, existingUser);
+            } else {
+                new User({ googleId: profile.id})
+                    .save()
+                    .then(user => done(null, user));
+            }
+        });
+    })
+);
