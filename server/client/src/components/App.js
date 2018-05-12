1a788aedd3e319123bf770089e3e46b789cab0c0
oleonort <oleonortt@gmail.com>
2018-05-12 20:07:13 +0200

    last changes

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -1,5 +1,22 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 
-const App = () => <div>Hi there!</div>;
+const Header = () => <h2>Header</h2>;
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveyNew = () => <h2>SurveyNew</h2>;
+const Landing = () => <h2>Landing</h2>;
+
+const App = () => (
+  <div>
+    <BrowserRouter>
+      <div>
+        <Header />
+        <Route exact path='/' component={Landing} />
+        <Route exact path='/surveys' component={Dashboard} />
+        <Route exact path='/surveys/new' component={SurveyNew} />
+      </div>
+    </BrowserRouter>
+  </div>
+);
 
 export default App;
