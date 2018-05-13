aee420c135747fa8b099875a07bed85ea2483090
oleonort <oleonortt@gmail.com>
2018-05-13 11:52:49 +0200

    redux-thunk + actions

---
+++ server/client/src/actions/index.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+import { FETCH_USER } from './types';
+
+export const fetchUser = () => dispatch => {
+  axios.get('/api/current_user').then(response => dispatch({
+    type: FETCH_USER,
+    payload: response
+  }));
+};
---
+++ server/client/src/components/Header.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render () {
+    return (
+      <nav>
+       <div className="nav-wrapper">
+         <a href="#" className="brand-logo">Emaily</a>
+         <ul id="nav-mobile" className="right">
+           <li><a href="#">Login With Google</a></li>
+         </ul>
+       </div>
+      </nav>
+    );
+  }
+}
+
+export default Header;
