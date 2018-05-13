1d3571f9ba264f2916c20266339fd55cd02fa60d
oleonort <oleonortt@gmail.com>
2018-05-13 19:40:32 +0200

    working header

--- server/client/src/actions/index.js
+++ server/client/src/actions/index.js
@@ -1,9 +1,11 @@
 import axios from 'axios';
 import { FETCH_USER } from './types';
 
-export const fetchUser = () => dispatch => {
-  axios.get('/api/current_user').then(response => dispatch({
+export const fetchUser = () => async dispatch => {
+  const response = await axios.get('/api/current_user');
+
+  dispatch({
     type: FETCH_USER,
-    payload: response
-  }));
+    payload: response.data
+  });
 };
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -5,15 +5,21 @@ module.exports = (app) => {
     scope: ['profile', 'email'],
   }));
 
-  app.get('/auth/google/callback', passport.authenticate('google'));
+  app.get(
+    '/auth/google/callback',
+     passport.authenticate('google'),
+     (requst, response) => {
+       response.redirect('/surveys');
+     }
+   );
 
   app.get('/api/logout', (requst, response) => {
     // logout is attached automatically by passport
     requst.logout();
-    response.send(requst.user);
+    response.redirect('/');
   });
 
-  app.get('/api/current-user', (requst, response) => {
+  app.get('/api/current_user', (requst, response) => {
     response.send(requst.user);
   });
 };
